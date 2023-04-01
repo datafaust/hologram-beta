03f593b9a29ded3d7dd5e283ff16a34a6d936b05
Fausto Lopez <faustolopez110@gmail.com>
2023-04-01 17:45:15 -0400

    try local

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ReactPlayer from "react-player";
 import classes from './app.module.css';
 //import YouTube from 'react-youtube';
 
-const api = "http://192.168.0.128:3002/get_song/"
+//const api = "http://192.168.0.128:3002/get_song/"
 //const api = "handler:3002/get_song/"
-//const api = "http://localhost:3002/get_song/"
+const api = "http://localhost:3002/get_song/"
 
 function App() {
 
