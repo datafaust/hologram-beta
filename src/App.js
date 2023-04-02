f2210716a005cd65946619766093ea5315847f09
Fausto Lopez <faustolopez110@gmail.com>
2023-04-02 01:01:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactPlayer from "react-player";
+//import ReactPlayer from "react-player";
 import classes from './app.module.css';
 //import YouTube from 'react-youtube';
 
@@ -10,20 +10,20 @@ const api = "http://172.18.0.7:3002/get_song/"
 function App() {
 
 
-  const [link, setLink] = useState([]);
+  //const [link, setLink] = useState([]);
   const [id, setId] = useState([]);
   //const [color, setColor] = useState('white')
 
 
-  const getSong = async () => {
-    // API to fetch some dummy data
-    fetch(api)
-      .then((resp) => resp.json())
-      .then((apiData) => {
-        console.log(apiData[0].link)
-        setLink(apiData[0].link);
-      });
-  }
+  // const getSong = async () => {
+  //   // API to fetch some dummy data
+  //   fetch(api)
+  //     .then((resp) => resp.json())
+  //     .then((apiData) => {
+  //       console.log(apiData[0].link)
+  //       setLink(apiData[0].link);
+  //     });
+  // }
 
   const getId = async () => {
     // API to fetch some dummy data
