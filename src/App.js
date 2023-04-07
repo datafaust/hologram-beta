e4de26e341ff00f7f86eb96878bc94dde75c0f0f
Fausto Lopez <faustolopez110@gmail.com>
2023-04-07 12:07:32 -0400

    fix streaming

--- src/App.js
+++ src/App.js
@@ -14,17 +14,8 @@ const start = "20";
 
 function App() {
 
-
-  //const [link, setLink] = useState([]);
-  // const [state, setState] = useState({});
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [id, setId] = useState(false);
-  // const [source, setSource] = useState([]);
   const [song, setSong] = useState([])
   // const [videoId, setVideoId] = useState(null)
-  // const [show, setShow] = useState(false)
-  
-  //const [color, setColor] = useState('white')
 
       const getSong = async () => {
        
@@ -37,31 +28,16 @@ function App() {
         .catch((error) => {
           console.log(error)
         });
-        
-      }
 
-    //   const fetchData = async () => {
-    //     try {
-    //         const response = await fetch(`${api}/get_song`);
-    //         const json = await response.json();
-    //         console.log(json[0]);
-    //         setSong(json[0]);
-    //     } catch (error) {
-    //         console.log("error", error);
-    //     }
-    // };
+      }
 
 
   useEffect(() => {
-     //fetchData()
      getSong()
 
     try {
       setInterval(async () => {
         getSong()
-        //getId();
-        //getSong();
-        //fetchData();
       }, 6000);
     } catch (e) {
       console.log(e);
@@ -79,82 +55,3 @@ function App() {
 }
 
 export default App;
-
-//song.length && <YoutubePlayer song={song} start={start}/>
-//   const getSong = async () => {
-//     await fetch(`${api}/get_song`)
-//       .then((resp) => resp.json())
-//       .then((apiData) => {
-//         console.log(apiData[0])
-//         //console.log(apiData[0].song_url)
-//         setSong(apiData[0]);
-//         setSource(apiData[0].source);
-//         setId(apiData[0].song_id)
-//         setVideoId({ videoId : song.song_url  })
-//         return setSong[0]
-//       })
-//   }
-
-//   const playVideo = (e, videoId) => {
-//     e.preventDefault()
-//     setVideoId({ videoId : song.song_url  })
-//     console.log(videoId)
-//   }
-
-
-
-//   useEffect(() => {
-
-//     // on launch get information from table
-//     getSong()
-
-//     // repeat call every 6 seconds from table for song information
-//     try {
-//       setInterval(async () => {
-//       //  getSong()
-//         if( source === 'streamer') {
-//           console.log('make streamer link...')
-        
-//         }
-//       }, 6000);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//    }, []
-//   ); // Dependency-array
-
-
-//   render(
-//     <div>{song &&
-//       <div>
-//       { 
-//         source === 'youtube' ? <YoutubePlayer id={song.id} start={start} />:<StreamPlayer song = {song} />
-//       }
-//       </div>
-//     }</div>
-//   )
-// }
-
-// export default App;
-
-
-
-// // <ReactPlayer
-// //             playing={true}
-// //             muted={true}
-// //             url={link}
-// //             width="80%"
-// //             height="50%"
-// //             controls={true}
-// //           />
-
-
-//   // if(source === 'youtube') {
-
-//   //   return <YoutubePlayer id={song.id} start={start} />
-
-//   // } else {
-    
-//   //   return song && <StreamPlayer song = {song} />
-
-//   // }
--- src/components/StreamPlayer.jsx
+++ src/components/StreamPlayer.jsx
@@ -4,62 +4,46 @@ import classes from './streamplayer.module.css';
 const StreamPlayer = ({ song }) => {
 
   
-  const videoRef = useRef(null)
+  // const videoRef = useRef(null)
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.pause()
-      videoRef.current.removeAttribute('src')
-      videoRef.current.load()
-    }
-  })
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     // videoRef.current.pause()
+  //     // videoRef.current.removeAttribute('src')
+  //     videoRef.current.load()
+  //   }
+  // })
 
   // console.log('passed song url: ' + song.song_url)
   const api = "http://dejavu:3002"
 
 
+
   return (
     <div className={classes.container}>
       <div className={classes.linktext}>{ `Current ${song.source} Link: ${song.song_url}` }</div>
+      
       <div className={classes.top}>
-        {song && <iframe
-          title="top"
-          width="150"
-          src={`${api}/videos/${song.song_url}`}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" >
-        </iframe>
+        {
+          song && <video width="150" autoPlay muted src={`${api}/videos/${song.song_url}#t=20`} />
         }
       </div>
       <div className={classes.bottom}>
-        {song && <iframe
-          title="bottom"
-          width="150"
-          src={`${api}/videos/${song.song_url}`}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" >
-        </iframe>}
-      </div>
-      <div className={classes.right}>
-        {song && <iframe
-          title="left"
-          width="150"
-          src={`${api}/videos/${song.song_url}`}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" >
-        </iframe>}
-      </div>
-      <div className={classes.left}>
-        {song &&  <iframe
-          title="left"
-          width="150"
-          src={`${api}/videos/${song.song_url}`}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" >
-        </iframe>
-
+        {
+          song && <video width="150" autoPlay muted src={`${api}/videos/${song.song_url}#t=20`} />
         }
       </div>
+      <div className={classes.right}>
+        {
+          song && <video width="150" autoPlay muted src={`${api}/videos/${song.song_url}#t=20`} />
+        }
+      </div>
+      <div className={classes.left}>
+        {
+          song && <video width="150" autoPlay muted src={`${api}/videos/${song.song_url}#t=20`} />
+        }
+      </div>
+     
     </div>
   )
 }
@@ -68,8 +52,8 @@ export default StreamPlayer
 
 
           
-
-
+// ${api}/videos/${song.song_url}
+// http://dejavu:3002/videos/Bee-Gees--Staying-Alive.mp4
 
 
 // <iframe
